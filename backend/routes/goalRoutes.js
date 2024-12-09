c6bc3c4d563b491e35cde257b187be56c9944804
AbdimalikAbukar <n01630627@humbermail.ca>
2024-12-08 19:03:33 -0500

    fixed

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../authMiddleware");
-const { addGoal, getGoals, shareGoal } = require("../controllers/goal");
+const {
+  addGoal,
+  getGoals,
+  shareGoal,
+  getSharedGoals,
+} = require("../controllers/goal");
 const { check } = require("express-validator");
 
 // Import Goals Mongoose Schema
 let Goal = require("../models/goal");
+const user = require("../models/user");
 
-//router.use(authMiddleware);
+router.use(authMiddleware);
 
 // Middleware for validating form parameters
 const validateGoal = [
@@ -19,21 +25,22 @@ const validateGoal = [
 // Attach goal routes to router
 
 // Goals route
-router.route("/").get((req, res) => {
-  // Query MongoDB for goals
-  Goal.find({ user: req.user.id }) // Return goals belonging to user
-    .then((goals) => {
-      console.log("Fetched Goals:", goals);
-      // Pass goals to view_goals
-      res.render("view_goals", {
-        title: "Goals",
-        goals: goals,
-      });
-    })
-    .catch((err) => {
-      console.log("Error fetching goals:", err);
-      res.status(500).send("Error fetching goals");
+router.route("/").get(async (req, res) => {
+  try {
+    // Fetch goals for the authenticated user
+    const goals = await Goal.find({ user: req.user.id });
+
+    // Pass goals and the username to the template
+    res.render("view_goals", {
+      title: "Goals", // Title for the page
+      goals: goals, // Goals retrieved from the database
+      user: req.user.username, // Username from the decoded JWT token
     });
+    console.log(user);
+  } catch (err) {
+    console.log("Error fetching goals:", err);
+    res.status(500).send("Error fetching goals");
+  }
 });
 
 // Add goals route
