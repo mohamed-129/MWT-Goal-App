e879325a18f63cd9d804d25ee62ab1d518b59358
mohamed-129 <mohamedidris129@gmail.com>
2024-12-12 21:20:16 -0500

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -16,19 +16,19 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      unique: true,
     },
   },
   { timestamps: true }
 );
 
-//hash password
+
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-// Method to compare passwords during login
+
 userSchema.methods.comparePassword = async function (inputPassword) {
   return bcrypt.compare(inputPassword, this.password);
 };
